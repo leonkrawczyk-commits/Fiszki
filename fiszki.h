0ffefc905dfa4a78686a6b0f7cf228901b44b1a8
Leon Krawczyk <s01197959@VDDL009.std.fizyka.pw.edu.pl>
2026-01-14 23:35:33 +0100

    test

---
+++ fiszki.h
@@ -0,0 +1,14 @@
+#ifndef _FISZKI_
+#define _FISZKI_
+#include <iostream>
+
+class fiszki{
+    public:
+    fiszki::fiszki(std::string f1, std::string b1);
+
+    private:
+    std::string f;
+    std::string b;
+};
+
+#endif
